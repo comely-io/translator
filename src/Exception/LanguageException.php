52a2410fa26390ba8e5e8acc8edc6f2eee469532
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-26 13:41:55 +0500

    TranslatorException

---
+++ src/Exception/LanguageException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "comely-io/translator" package.
+ * https://github.com/comely-io/translator
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comely-io/translator/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Translator\Exception;
+
+/**
+ * Class LanguageException
+ * @package Comely\Translator\Exception
+ */
+class LanguageException extends TranslatorException
+{
+}
